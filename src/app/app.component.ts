1ccbea0ecc2ce5c2ce63a0b3e43f63ed09f1a7f6
marekboro <marek.borowicz83@gmail.com>
2021-01-28 15:24:26 +0000

    added nav bar with bootstrap css, and another component to play aroudn with

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,60 +1,89 @@
 import { Component } from '@angular/core';
-import { GitHubService } from './github.service'; // IMPORT service
-import { FormControl } from '@angular/forms';
-import { filter, debounceTime, distinctUntilChanged } from 'rxjs/operators'
-
 
 @Component({
   selector: 'app-root',
-  template: `
-         <input class= "form-control" type="search" [formControl] = "searchControl">
-          <h3> User Results:  </h3>
-          <div *ngIf="isLoading"> 
-            <i class="fa fa-spinner fa-spin fa-6x"> </i>
-          </div>
-          <div *ngFor="let user of users" class="media">
-            <div class="media"> 
-              <a href="{{ user.html_url }}" > 
-                <img src="{{user.avatar_url}}" class = "mr-4" alt ="..." width ="64" height = "64"> 
-              </a>
-              <div class="media-body">
-                <h5 class ="mt-0">{{user.login}}</h5>
-                Score: {{user.score}}
-              </div>
-            </div>
-          </div>
+  template: `  
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" routerLink="">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" routerLink="GitHub">GitHub</a>
+        </li>           
+        <li class="nav-item">
+          <a class="nav-link" routerLink="Tester">Tester</a>
+        </li>   
+      </ul>    
+    </div>
+  </nav>
+  <router-outlet></router-outlet>                                                                      
+
           `,
-  providers: [GitHubService] // we register the service so the injector knows where to find it ? 
+  providers: []
+
 
 })
-export class AppComponent {                             // add a consttuctor and INJECT the service into the constructor.
-  searchControl = new FormControl();
-  isLoading = false;
-  users = [];
-  constructor(private _githubService: GitHubService) {  // injection is here. 
-  }
-
-  ngOnInit() {
-    this.searchControl.valueChanges
-      .pipe(
-        filter(text => text.length >= 3),
-        debounceTime(400),
-        distinctUntilChanged()
-      ).subscribe(value => {
-        this.isLoading = true;
-        this._githubService.getGitHubData(value)
-          .subscribe(data => {
-            this.isLoading = false;
-            this.users = this.users.concat(data.items)
-          });
-
-
-      });
+export class AppComponent {
+  constructor() {
 
   }
 
 }
 
+
+
+// @Component({
+//   selector: 'app-root',
+//   template: `
+//          <input class= "form-control" type="search" [formControl] = "searchControl">
+//           <h3> User Results:  </h3>
+//           <div *ngIf="isLoading"> 
+//             <i class="fa fa-spinner fa-spin fa-6x"> </i>
+//           </div>
+//           <div *ngFor="let user of users" class="media">
+//             <div class="media"> 
+//               <a href="{{ user.html_url }}" > 
+//                 <img src="{{user.avatar_url}}" class = "mr-4" alt ="..." width ="64" height = "64"> 
+//               </a>
+//               <div class="media-body">
+//                 <h5 class ="mt-0">{{user.login}}</h5>
+//                 Score: {{user.score}}
+//               </div>
+//             </div>
+//           </div>
+//           `,
+//   providers: [GitHubService] // we register the service so the injector knows where to find it ? 
+
+// })
+// export class AppComponent {                             // add a consttuctor and INJECT the service into the constructor.
+//   searchControl = new FormControl();
+//   isLoading = false;
+//   users = [];
+//   constructor(private _githubService: GitHubService) {  // injection is here. 
+//   }
+
+//   ngOnInit() {
+//     this.searchControl.valueChanges
+//       .pipe(
+//         filter(text => text.length >= 3),
+//         debounceTime(400),
+//         distinctUntilChanged()
+//       ).subscribe(value => {
+//         this.isLoading = true;
+//         this._githubService.getGitHubData(value)
+//           .subscribe(data => {
+//             this.isLoading = false;
+//             this.users = this.users.concat(data.items)
+//           });
+
+
+//       });
+
+//   }
+
+// }
 
 // export class AppComponent {                             // add a consttuctor and INJECT the service into the constructor.
 //   constructor(private _githubService: GitHubService) {  // injection is here. 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http'
 import { ReactiveFormsModule } from '@angular/forms'
+import { HomeComponent } from "./home.component"
+import { NotFoundComponent } from "./notfound.component"
+import { GitHubComponent } from "./github.component"
+import { routing } from "./app.routing"
+import { TesterComponent } from './another.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent,
+    GitHubComponent,
+    TesterComponent
+
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]

c603160ecefb954dc8c202c5ce33a62cbc139923
marekboro <marek.borowicz83@gmail.com>
2021-01-28 10:44:22 +0000

    injected a dependancy into app component via constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { GitHubService } from './github.service'; // IMPORT service
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: ``,
+  providers: [GitHubService] // we register the service so the injector knows where to find it ? 
+
 })
-export class AppComponent {
-  title = 'angularRESTandObserve';
+export class AppComponent {                             // add a consttuctor and INJECT the service into the constructor.
+  constructor(private _githubService: GitHubService) {  // injection is here. 
+    this._githubService.getGitHubData('greg')           // we then call the getGHD method with a parameter in the _githubS instance and...
+      .subscribe(data => console.log(data));            // SUBSCRIBE TO IT. 
+  }
+
 }

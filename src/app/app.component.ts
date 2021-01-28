79cdeaeb9d28e9acc1ba0cb7705a4ba61a31dfb3
marekboro <marek.borowicz83@gmail.com>
2021-01-28 12:19:24 +0000

    ch9 done, added search to the user fetch from github to make this app dynamic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,66 @@
 import { Component } from '@angular/core';
 import { GitHubService } from './github.service'; // IMPORT service
+import { FormControl } from '@angular/forms';
+import { filter, debounceTime, distinctUntilChanged } from 'rxjs/operators'
 
 
 @Component({
   selector: 'app-root',
-  template: ``,
+  template: `
+         <input class= "form-control" type="search" [formControl] = "searchControl">
+          <h3> User Results:  </h3>
+          <div *ngIf="isLoading"> 
+            <i class="fa fa-spinner fa-spin fa-6x"> </i>
+          </div>
+          <div *ngFor="let user of users" class="media">
+            <div class="media"> 
+              <a href="{{ user.html_url }}" > 
+                <img src="{{user.avatar_url}}" class = "mr-4" alt ="..." width ="64" height = "64"> 
+              </a>
+              <div class="media-body">
+                <h5 class ="mt-0">{{user.login}}</h5>
+                Score: {{user.score}}
+              </div>
+            </div>
+          </div>
+          `,
   providers: [GitHubService] // we register the service so the injector knows where to find it ? 
 
 })
 export class AppComponent {                             // add a consttuctor and INJECT the service into the constructor.
+  searchControl = new FormControl();
+  isLoading = false;
+  users = [];
   constructor(private _githubService: GitHubService) {  // injection is here. 
-    this._githubService.getGitHubData('greg')           // we then call the getGHD method with a parameter in the _githubS instance and...
-      .subscribe(data => console.log(data));            // SUBSCRIBE TO IT. 
+  }
+
+  ngOnInit() {
+    this.searchControl.valueChanges
+      .pipe(
+        filter(text => text.length >= 3),
+        debounceTime(400),
+        distinctUntilChanged()
+      ).subscribe(value => {
+        this.isLoading = true;
+        this._githubService.getGitHubData(value)
+          .subscribe(data => {
+            this.isLoading = false;
+            this.users = this.users.concat(data.items)
+          });
+
+
+      });
+
   }
 
 }
+
+
+// export class AppComponent {                             // add a consttuctor and INJECT the service into the constructor.
+//   constructor(private _githubService: GitHubService) {  // injection is here. 
+//     this._githubService.getGitHubData('greg')           // we then call the getGHD method with a parameter in the _githubS instance and...
+//       .subscribe(data => console.log(data.items));            // SUBSCRIBE TO IT. 
+// // .subscribe(data => console.log(data));            // SUBSCRIBE TO IT. 
+//   }
+
+// }
